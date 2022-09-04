a9168105ccf6cb37c7e605cc638bf08e7f2b42bf
Diniki <jayakodyjadn.19@uom.lk>
2022-09-04 07:25:29 +0530

    nav bar layout

--- src/Common_Components/NavBar.jsx
+++ src/Common_Components/NavBar.jsx
@@ -7,7 +7,14 @@ function NavBar()
     return(
         <>
             <div className={NavBarStyles.box}>
-
+                <table>
+                    <tr><button className={NavBarStyles.navBtn}>Foods</button></tr>
+                    <tr><button className={NavBarStyles.navBtn}>Rooms</button></tr>
+                    <tr><button className={NavBarStyles.navBtn}>Room Bookings</button></tr>
+                    <tr><button className={NavBarStyles.navBtn}>Employees</button></tr>
+                    <tr><button className={NavBarStyles.navBtn}>Payments</button></tr>
+                    <tr><button className={NavBarStyles.navBtn}>Customers</button></tr>
+                </table>
             </div>
         </>
     );
