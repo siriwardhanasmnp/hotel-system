ced2c1fd72bbefcc87dca1a023aaa99b2ccc77ed
Diniki <jayakodyjadn.19@uom.lk>
2022-09-04 07:57:23 +0530

    HomePage layout

--- src/Screens/HomePage.jsx
+++ src/Screens/HomePage.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 
 import HomePageStyles from './HomePage.module.css';
+
 import NavBar from '../Common_Components/NavBar'
+import Header from '../Common_Components/Header'
 
 function HomePage()
 {
     return(
         <>
         <div className={HomePageStyles.screen}>
+            <Header/>
             <NavBar/>
         </div>
         </>
