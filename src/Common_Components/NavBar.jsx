8bcf3f881e60b2092a21cbe746d4cee158e26b65
siriwardhanasmnp <siriwardhanasmnp.19@uom.lk>
2022-09-04 12:53:52 +0530

    Navbar

--- src/Common_Components/NavBar.jsx
+++ src/Common_Components/NavBar.jsx
@@ -1,4 +1,10 @@
 import React from "react";
+import food from "./Images/food.png";
+import customer from "./Images/Cus.png";
+import employee from "./Images/Emp.png";
+import payment from "./Images/Payment.png";
+import room from "./Images/Room.png";
+import addRooom from "./Images/Room plus.png";
 
 import NavBarStyles from './NavBar.module.css';
 
@@ -8,12 +14,12 @@ function NavBar()
         <>
             <div className={NavBarStyles.box}>
                 <table>
-                    <tr><button className={NavBarStyles.navBtn}>Foods</button></tr>
-                    <tr><button className={NavBarStyles.navBtn}>Rooms</button></tr>
-                    <tr><button className={NavBarStyles.navBtn}>Room Bookings</button></tr>
-                    <tr><button className={NavBarStyles.navBtn}>Employees</button></tr>
-                    <tr><button className={NavBarStyles.navBtn}>Payments</button></tr>
-                    <tr><button className={NavBarStyles.navBtn}>Customers</button></tr>
+                    <tr><button className={NavBarStyles.navBtn}><img src={food} width="30" height="30"/>Foods</button></tr>
+                    <tr><button className={NavBarStyles.navBtn}><img src={room} width="30" height="30"/>Rooms</button></tr>
+                    <tr><button className={NavBarStyles.navBtn}><img src={addRooom} width="30" height="30"/>Room Bookings</button></tr>
+                    <tr><button className={NavBarStyles.navBtn}><img src={employee} width="30" height="30"/>Employees</button></tr>
+                    <tr><button className={NavBarStyles.navBtn}><img src={payment} width="30" height="30"/>Payments</button></tr>
+                    <tr><button className={NavBarStyles.navBtn}><img src={customer} width="30" height="30"/>Customers</button></tr>
                 </table>
             </div>
         </>
